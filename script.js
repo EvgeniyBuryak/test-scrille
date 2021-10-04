7c77f0d16851c5fa6542588b4750ccbf7df7fe7b
Jhonah <buryakone@gmail.com>
2021-10-04 23:37:51 +0700

    первое задание сделано - наполовину самописно получилось

--- script.js
+++ script.js
@@ -24,15 +24,43 @@ function getPairs(arr, sum) {
         })*/
     //});
 
-        
+    
+    const hash  = {};    
+    arr.forEach(elem => { // запоминает количество похожих элементов массива
+        hash[elem] ? hash[elem] += 1 :hash[elem] = 1;
+    });
+    
+    //return hash;
+    /*
+    let counter = 0;
+    arr.forEach(elem => {
 
-    return arr.map( (value, index) => arr.slice(index + 1).map( secondValue => { 
-            if (value + secondValue == sum) {
-                return [value, secondValue];
+        let k = sum - elem;
+        if(k === 0 && hash[sum] > 1) {
+            counter++;
+        } else if (k !== 0 && hash[sum] > 0) {
+            counter++;
+        }
+    });
+    */
+    //return counter;
+
+    return arr.map( (firstV, index) => arr.slice(index + 1).map( secondV => { 
+            if (firstV + secondV == sum) {
+                hash[firstV] -= 1;
+                hash[secondV] -= 1;
+                if (hash[firstV] >= 0 & hash[secondV] >= 0)
+                    return [firstV, secondV];
             }
         }))
         .flat()
         .filter( pair => Array.isArray(pair) ); // выводит [[3, 2], [3, 2], [5, 0]], что неверно
+        //.filter( pair => {
+            //if (hash[pair[0]] < 0) { hash[pair[0]] += 1; return false; }
+            //else if (hash[pair[1]] < 0) { hash[pair[1]] += 1; return false; }
+            //else return true;
+        //});
+        //return hash;
         /*.filter( (pair, index) => {
             console.log(pair);
             console.log(index);
@@ -54,7 +82,7 @@ function getPairs(arr, sum) {
 const result = getPairs([22, 3, 5, 0, 2, 2], 5);
 console.log(result);
 console.log('result: [[3, 2], [5, 0]]');
-/*
+
 const result1 = getPairs([-5, 33, 2, 2, 3, 5, 0, 10, 3], 5);
 console.log(result1);
 console.log('result: [[-5, 10], [2, 3], [2, 3], [5, 0]]');
@@ -67,4 +95,3 @@ console.log('result: [[5, 0], [5, 0], [5, 0]]');
 const result3 = getPairs([3, 3, 6, 0], 6);
 console.log(result3);
 console.log('result: [[3, 3], [6, 0]]');
-*/
